f10f5a83f8b2334e3ebd552fe50b634c75c9fc89
SimoneZampieri <simonezampieri106@gmail.com>
2024-11-18 16:40:05 +0100

    consegna 2 completata

--- Consegna 2-Dadi/JS/script.js
+++ Consegna 2-Dadi/JS/script.js
@@ -1,1 +1,20 @@
-//generare un
+//generare un numero randomico da 1 a 6 per il giocatore e per il computer, stabilendo un vincitore in base al punteggio più alto 
+
+
+//impostiamo due variabili note come i numeri del giocatore e del computer rispettivamente
+
+let numGiocatore = Math.ceil(Math.random()*6)
+let numComputer = Math.ceil(Math.random()*6)
+
+//a seguito del tiro, appariranno i seguenti messaggi
+console.log( 'Player:' + ' ' + numGiocatore)
+console.log( 'Computer:' + ' ' + numComputer)
+let messaggio = 'hai perso';
+
+//ora definiamo le condizioni di gioco
+
+if( numGiocatore > numComputer){
+    console.log('Player Vince')
+} else if( numComputer > numGiocatore ){
+    console.log('Computer Vince')
+}
